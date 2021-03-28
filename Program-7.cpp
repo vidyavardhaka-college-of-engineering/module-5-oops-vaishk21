5942574038dac0a89061dd6b3cf7e5c9e1b43ca9
vaishk21 <kotavaish21@gmail.com>
2021-03-28 13:13:37 +0000

    dond

--- Program-7.cpp
+++ Program-7.cpp
@@ -11,8 +11,10 @@ int main()
     int n, fact = 1;
     std::cout << "Enter a positive integer: ";
     std::cin >> n;
-   // for(int i = 1; i <=n; ++i)
-    //{
+    for(int i=1;i<=n;++i)// for(int i = 1; i <=n; ++i)
+     {
+       fact=fact*i;
+     }
         //Fill the missing statement
     //}
     cout <<fact;    
